952828cb698e7181c5310a5bb8599b892868c540
Sam <sam@malpass.me.uk>
2020-03-02 16:26:52 +0000

    Add some comments

--- C-Lesson-3.cpp
+++ C-Lesson-3.cpp
@@ -1,15 +1,18 @@
 #include<stdio.h>
 
+// Declare and Define function
 int add(int a, int b) 
 {
 	return a + b;
 }
 
+// Declare and Define function
 int multiply(int a, int b)
 {
 	return (a * b);
 }
 
+// Declare and Define Recursive function
 int fib(int i)
 {
 	if (i == 0) {
@@ -20,16 +23,21 @@ int fib(int i)
 		return 1;
 	}
 	else {
+		// Recursive bit here
 		return fib(i - 1) + fib(i - 2);
 	}
 }
 
+// Declare and Define Recursive function
 int factorial(int i) {
 	if (i <= 1) {
 		return 1;
 	}
+	// Recursive bit here
 	return i * factorial(i - 1);
 }
+
+
 int main()
 {
 	int c = multiply(2, 5);
